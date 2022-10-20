ddc848e4c931ee71656379502b7869443350ace1
George Tian <tianbeiwen88@gmail.com>
2022-10-20 10:59:37 +0800

    rm test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-export HOOK_URL=https://hooks.slack.com/services/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
-
-send() {
-		DEFAULT=$@
-			TEXT=${DEFAULT:-"knock knock"}
-				curl -X POST --data-urlencode "payload={\"channel\": \"#notifications\", \"username\": \"webhookbot\", \"text\": \"$TEXT\", \"icon_emoji\": \":bulb:\"}" $HOOK_URL > /dev/null 2>&1
-			}
-python -c "print('hi')"; send test3
